ddc9ed973d13f699b6aeecab0e55bd1e06df45af
noimir <noa.zagar@gmail.com>
2026-05-06 21:00:40 +0200

    zuta
    Ruza

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.green,
+          backgroundColor: const Color.fromARGB(255, 201, 65, 47),
           title: const Text('App Bar'),
         ),
         body: Container(child: const Text('Hi there')),
