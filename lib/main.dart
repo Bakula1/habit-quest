09ce03a1f71c8ced68fb991faebc310571bcdd36
Antonio <bakula.antonio2006@gmail.com>
2026-05-08 00:04:28 +0200

    Dodaj package-lock za GitHub Actions

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.blue),
+      theme: ThemeData(primarySwatch: Colors.red),
       home: const WidgetTree(),
     );
   }
